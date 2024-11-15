a73b7186c55147c0bd40ece3411f414577df34ab
TheRev <cesarhumbertogarciafelix@gmail.com>
2024-11-15 13:06:16 -0700

    Practica terminada

---
+++ Controllers/PubishersController.cs
@@ -0,0 +1,32 @@
+﻿using CREAR_API.Data.Services;
+using CREAR_API.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CREAR_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private PublishersService _publishersService;
+        public PublishersController(PublishersService publishersService)
+        {
+            _publishersService = publishersService;
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddPublisher([FromBody] PublisherVM publisher)
+        {
+            _publishersService.AddPublisher(publisher);
+            return Ok();
+        }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _response = _publishersService.GetPublisherData(id);
+            return Ok(_response);
+        }
+    }
+}
---
+++ Data/Services/PublishersService.cs
@@ -0,0 +1,44 @@
+﻿using CREAR_API.Data.Models;
+using CREAR_API.Data.ViewModels;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+
+namespace CREAR_API.Data.Services
+{
+    public class PublishersService
+    {
+        private AppDbContext _context;
+        public PublishersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //metodo para crear autor
+        public void AddPublisher(PublisherVM publisher)
+        {
+            var _publisher = new Models.Publisher()
+            {
+                Name = publisher.Name
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+        }
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Titulo,
+                        BookAuthors = n.Book_Author.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+            return _publisherData;
+        }
+    }
+}
